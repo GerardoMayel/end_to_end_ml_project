5b76d6e65672cbc6899fa1881cef8ae2231cef3b
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2023-03-19 22:36:04 -0600

    EDA and Problem statements

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -22,7 +23,8 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-# remove to make the exception tests:
+# #
+# # remove to make the exception tests:
 # if __name__ == "__main__":
 #     try:
 #         a = 1 / 0
